d4c5a99f7eac77b946d8725ed78454739ef7956c
JoseJorgeGC <jjgutierrez910@gmail.com>
2023-03-26 18:50:49 -0400

    Config de la DB mysql, y config de archivos staticos, MEDIA_ROOT y MEDIA_URL.

--- django_junk/settings.py
+++ django_junk/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_junk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'junker',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'inventary/media'
+MEDIA_URL = '/inventary/media/'
